e8ec30119a3f49227eba45c5a472ca6dd7545834
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-02-24 21:06:50 +0000

    linked mongodb to flask

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config.from_pyfile('env.py')
+app.config["MONGODB_NAME"] = os.environ.get('MONGODB_NAME')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/get_reviews')
+def get_reviews():
+    return render_template("reviews.html", reviews=mongo.db.reviews.find())
 
 
 if __name__ == '__main__':
---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault("MONGODB_NAME", "review_site")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://cthomson:Amberley050493@bookcluster-s2l6a.mongodb.net/review_site?retryWrites=true&w=majority")
+
+
