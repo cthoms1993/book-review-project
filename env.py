74c8b03982fd43e33d5917d57f4524f5475bd7fd
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-02-29 20:18:51 +0000

    added base.html for template inheritance.

--- env.py
+++ env.py
@@ -2,5 +2,3 @@ import os
 
 os.environ.setdefault("MONGODB_NAME", "review_site")
 os.environ.setdefault("MONGO_URI", "mongodb+srv://chris:Thomson1993@cluster0-mi1fy.mongodb.net/review_site?retryWrites=true&w=majority")
-
-
