27c4afb986b2ebd5b1039e1463ce9c75f2d64609
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-03-06 21:53:38 +0000

    added log in page - encoding error when signing in.

--- app.py
+++ app.py
@@ -15,11 +15,25 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/login_page')
 def login_page():
     if 'username' in session:
-        return 'you are logged in as' + session['username']
+        return 'You are logged in as ' + session['username']
+
     return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name': request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user[
+            'password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+    return 'Invalid username/password combination'
 
 
 @app.route('/register', methods=['POST', 'GET'])
@@ -31,14 +45,11 @@ def register():
             hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
             users.insert({'name': request.form['username'], 'password': hashpass})
             session['username'] = request.form['username']
-            return redirect(url_for(login_page))
+            return redirect(url_for('register'))
+
         return 'that username already exists'
+
     return render_template('register.html')
-
-
-@app.route('/login')
-def login():
-    return ''
 
 
 @app.route('/get_reviews')
